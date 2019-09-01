e0921ae172a9952d12409957f9f464535ed36133
Ut Huynh <ut.huynhvn@gmail.com>
2019-09-01 15:55:12 +0700

    init + add files

---
+++ bin/run.bat
@@ -0,0 +1,6 @@
+call USH.exe bms2 30000 0 PA 0
+call USH.exe bms2 30000 0 P 0
+call USH.exe bms2 30000 0 R 5000
+pause
+
+Example command: call USH.exe [dataFileName:0] [minUtil:1] [maxLength:2] [alg:M|PA|P|R:3] [upsilon:4]
